5f565957fded72bc7ea7afc5f87f5f7b79bd4ba8
ehab <ehabraafat808@gmail.com>
2024-09-24 22:33:26 +0200

    .

---
+++ unit 4/unit4_lesson2_CA_1/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 20, 2024
+ */
+#include "CA.h"
+void setup()
+{
+	// init all the Drivers
+	// init IRQ ....
+	//init HAL US_Driver DC_Driver
+	//init block
+	//set States pointers for each block
+	CA_state=STATE(CA_waiting);
+}
+
+void main()
+{
+setup();
+while(1)
+{
+	CA_state();
+}
+}
---
+++ unit 4/unit4_lesson2_CA_V2/DC.h
@@ -0,0 +1,27 @@
+/*
+ * CA.h
+ *
+ *  Created on: Aug 20, 2024
+ */
+
+#ifndef DC_H_
+#define DC_H_
+#include "state.h"
+#include "stdio.h"
+#include "stdlib.h"
+//Define State
+enum
+{
+	DC_idle,
+	DC_busy,
+}DC_State_ID ;
+
+//Declare state functions CA
+STATE_define(DC_idle);
+STATE_define(DC_busy);
+void DC_init();
+//STATE pointer to function
+extern void (*DC_state)();
+
+
+#endif /* DC_H_ */
